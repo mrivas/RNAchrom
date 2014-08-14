64b7acc1abb742da62effd65b4ea9f6ccbc1cbeb
mrivas <mrivas@users.noreply.github.com>
2014-08-13 22:59:22 -0700

    Delete redundant files

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(name='funniest',
-	version='0.1',
-	description='RNA-chromatin interaction tools',
-	author='Marcelo Rivas-Astroza',
-	license='GPL',
-	packages=['RNAchrom'],
-	zip_safe=False)
