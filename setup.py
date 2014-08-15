67d95b7725981716b7fc8fbd2ad47c33e6c4a58e
Marcelo Rivas <marcelorivas@gmail.com>
2014-08-14 18:57:19 -0700

    Add dependencies to setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from setuptools import setup
 
-setup(name='funniest',
+setup(name='RNAchrom',
 	version='0.1',
 	description='Tools for RNA-chromatin interactions',
-	url='https://github.com/mrivas/RNAchrom',
+	url='http://mrivas.hasdocs.com/RNAchrom/',
 	author='Marcelo Rivas-Astroza',
 	license='GPL',
 	packages=['RNAchrom'],
-	scripts=['bin/annotateBAM.py'],
-	install_requires=['argparse','sys','pysam','HTSeq','numpy','re'],
+	entry_points={'console_scripts':['annotateBAM=annotateBAM.command_line:main'],},
+	install_requires=['numpy','sys','argparse','HTSeq','scipy',],
+	dependency_links=['https://github.com/noteed/python-pickle/archive/master.zip'],
 	zip_safe=False)
