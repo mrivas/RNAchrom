8a7c8cf94a0e6b2c77c1da9d982db75601cc4310
mrivas <mrivas@users.noreply.github.com>
2014-09-10 15:44:18 -0700

    Remove py extention from scripts

--- RNAchrom/classes.py
+++ RNAchrom/classes.py
@@ -1,7 +1,14 @@
+class dotDict(dict):
+	def __getattr__(self, attr):
+		return self.get(attr, None)
+	__setattr__= dict.__setitem__
+	__delattr__= dict.__delitem__
+########################################
 class annotReader:
 	
 	def __init__(self, annotFile):
 		self.annotFile = open(annotFile,'r')
+		self.mate1, self.mate2 = dotDict,dotDict
 		self.lineNumber = -1 # dummy initial value to be used on __getitem__'s "if" stament 
 
 	def __getitem__(self,index):
@@ -10,19 +17,21 @@ class annotReader:
 		if self.annotFile.tell()==self.lineNumber:
 			self.annotFile.seek(0)
 		self.lineNumber   = self.annotFile.tell()
-		self.firstIV      = HTSeq.GenomicInterval(line[0],int(line[1]),int(line[2]),line[3] )
-		self.firstGenes   = line[4].split('|')[0].split(',')
-		self.firstExon    = line[4].split('|')[1]
-		self.firstRepeat  = line[4].split('|')[2]
-		self.firstSJs     = eval( line[5] )
-		self.secondIV     = HTSeq.GenomicInterval(line[6],int(line[7]),int(line[8]),line[9] )
-		self.secondGenes  = line[10].split('|')[0].split(',')
-		self.secondExon   = line[10].split('|')[1]
-		self.secondRepeat = line[10].split('|')[2]
-		self.secondSJs    = eval( line[11] )
-		self.dist         = min( abs( int(line[2])-int(line[7]) ), abs( int(line[1])-int(line[8]) ) )
-		self.readName     = line[12]
-		return self	
+		self.mate1.iv      = HTSeq.GenomicInterval(line[0],int(line[1]),int(line[2]),line[3] )
+		self.mate1.genes   = line[4].split('|')[0].split(',')
+		self.mate1.exon    = line[4].split('|')[1]
+		self.mate1.repeat  = line[4].split('|')[2]
+		self.mate1.sjs     = eval( line[5] )
+		self.mate1.dist    = min( abs( int(line[2])-int(line[7]) ), abs( int(line[1])-int(line[8]) ) )
+		self.mate1.readName = line[12]
+		self.mate2.iv     = HTSeq.GenomicInterval(line[6],int(line[7]),int(line[8]),line[9] )
+		self.mate2.genes  = line[10].split('|')[0].split(',')
+		self.mate2.exon   = line[10].split('|')[1]
+		self.mate2.repeat = line[10].split('|')[2]
+		self.mate2.sjs    = eval( line[11] )
+		self.mate2.dist   = min( abs( int(line[2])-int(line[7]) ), abs( int(line[1])-int(line[8]) ) )
+		self.mate2.readName = line[12]
+		return [self.mate1,self.mate2]	
 		
 #########################################	
 class interactions():
@@ -99,19 +108,19 @@ class links:
 awareMates=RNAchrom.annotReader(awareAnnot)
 awareLinks=RNAchrom.links()
 interactions=RNAchrom.interactions(windSize)
-for mates in awareMates:
-	if not( len(mates.secondGenes)==1 and mates.secondGenes!="." and mates.secondExon=="exon" and mates.firstSJs==-1 and mates.dist>dist ):
+for mate1,mate2 in awareMates:
+	if not( len(mate2.genes)==1 and mate2.genes!=['.'] and mate2.exon=="exon" and mate1.sjs==[-1] and mate1.dist>dist ):
 		continue 
-	interactions[ mates.secondGenes[0] ] = mates.firstIV
-	awareLinks[   mates.readName       ] = [mates.secondGenes[0], mates.firstIV ]
+	interactions[ mate2.genes[0]] = mates1.iv
+	awareLinks[ mate1.readName  ] = [mate2.genes[0], mate1.iv ]
 # Build blindLinks and add inferred aware-links to awareLinks
 blindMates=RNAchrom.annotReader(blindAnnot)
 blindLinks=RNAchrom.links()
-for mates in blindMates:
-	if not( len(mates.secondGenes)==1 and mates.secondGenes!="." and mates.secondExon=="exon" and mates.firstSJs==-1 and mates.dist>dist ):
+for mate1,mate2 in blindMates:
+	if not( len(mate2.genes)==1 and mate2.genes!=['.'] and mate2.exon=="exon" and mate1.sjs==[-1] and mate1.dist>dist ):
 		continue 
-	blindLinks[mates.readName ] = [mates.secondGenes[0], mates.firstIV  ]
-	blindLinks[mates.readName ] = [mates.firstGenes[0] , mates.secondIV ]
+	blindLinks[mate1.readName ] = [mate2.genes[0], mate1.iv  ]
+	blindLinks[mate1.readName ] = [mate1.genes[0], mate2.iv ]
 
 # Count hits on targets
 for aimerGene, targetIV in interactions:
