336cab8671dd8601ba472d57e10d6b009e6cb6cc
mrivas <mrivas@users.noreply.github.com>
2016-09-15 09:31:23 -0700

    addGO

--- setup.py
+++ setup.py
@@ -7,6 +7,6 @@ setup(name='RNAchrom',
 	author='Marcelo Rivas-Astroza',
 	license='GPL',
 	packages=['RNAchrom'],
-	scripts=['bin/annotateBAM','bin/countHits','bin/corrWind','bin/sj','bin/specificity','bin/removeOverlapps','bin/detectLinks','bin/filterBAM','bin/countReads','bin/bamFragDistribution','bin/fixBAMmates','bin/countFragments','bin/splitBAM','bin/countReadTypes','bin/geneTargets','bin/countRestrSites','bin/plotCounts','bin/connections','bin/annotateConn','bin/vennDiagram'],
+	scripts=['bin/annotateBAM','bin/countHits','bin/corrWind','bin/sj','bin/specificity','bin/removeOverlapps','bin/detectLinks','bin/filterBAM','bin/countReads','bin/bamFragDistribution','bin/fixBAMmates','bin/countFragments','bin/splitBAM','bin/countReadTypes','bin/geneTargets','bin/countRestrSites','bin/plotCounts','bin/connections','bin/annotateConn','bin/vennDiagram','bin/geneNames','bin/annotateInteractions','bin/countInteractions','bin/addGO'],
 	install_requires=['numpy','pysam','argparse','HTSeq','scipy','pybedtools','pandas',],
 	zip_safe=False)
